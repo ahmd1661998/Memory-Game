9eab312659cee195cfe4972792a84bf806b3789d
Ahmed Osama <ahmedOsamaBedawy@users.noreply.github.com>
2019-08-08 18:04:00 +0200

    add Function-checkCard

--- js/app.js
+++ js/app.js
@@ -1,3 +1,76 @@
+/**
+ * 
+ * =====> Varibals
+ **/
+
+const deck = document.querySelector(".deck");
+const card = document.querySelectorAll(".card");
+
+let toggleCards = [];
+
+function toggleCard(clickTarget) {
+    clickTarget.classList.toggle('open');
+    clickTarget.classList.toggle('show');
+}
+
+function addToggleCard(clickTarget) {
+    toggleCards.push(clickTarget);
+}
+
+function checkForMatch() {
+    if (toggleCards[0].firstElementChild.className === toggleCards[1].firstElementChild.className) 
+    {
+        toggleCards[0].classList.toggle('match');
+        toggleCards[1].classList.toggle('match');
+        toggleCards = [];
+    } else {
+        setTimeout (() => {
+            toggleCard(toggleCards[0]);
+            toggleCards(toggleCards[1]);
+            toggleCards = [];
+        }, 1000);
+    }
+}
+
+deck.addEventListener('click', event => {
+    const clickTarget = event.target;
+    if(clickTarget.classList.contains('card') && toggleCards.length < 2) {
+        toggleCard(clickTarget);
+        addToggleCard(clickTarget);
+        if(toggleCards.length === 2) {
+            checkForMatch(clickTarget);
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
