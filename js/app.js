e4501b7478866cec645d56b219190cbc703eeec7
Ahmed Osama <ahmedOsamaBedawy@users.noreply.github.com>
2019-08-10 16:00:30 +0200

    edits

--- js/app.js
+++ js/app.js
@@ -1,27 +1,135 @@
-/**
- * 
- * =====> Varibals
- **/
-
+/* ==================== Global Varibals ==================== */
 const deck = document.querySelector(".deck");
 const card = document.querySelectorAll(".card");
+let toggleCards = [];
+let matched = 0;
+const TOTAL_PAIRS = 8;
+
 const restart = document.querySelector('.restart');
+
 const cancel = document.querySelector('.modal-cancel');
 const replay = document.querySelector('.modal-replay');
+
 let moves = 0;
+
 let clockOff = true;
 let time = 0;
 let clockId;
-let matched = 0;
-const TOTAL_PAIRS = 8;
-let toggleCards = [];
 
-////////////////////////////////////////////////////////////////
-/**
- * 
- * =====> toggleCard
- **/
 
+/* ==================== shuffle ==================== */
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function shuffleDeck() {
+    const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
+    const shuffledCards = shuffle(cardsToShuffle);
+    for (const card of shuffledCards) {
+        deck.appendChild(card);
+    }
+}
+
+/* ==================== removeStar ==================== */
+function removeStar() {
+    const starList = document.querySelectorAll('.stars li');
+    for (const star of starList) {
+        if (star.style.display !== 'none') {
+            star.style.display = 'none'
+            break;
+        }
+    }
+}
+
+function checkScore() {
+    if (moves === 16 || moves === 24) {
+        removeStar();
+    }
+}
+
+/* ==================== moves ==================== */
+function addMove() {
+    moves++;
+    const movesText = document.querySelector('.moves');
+    movesText.innerHTML = moves;
+}
+
+/* ==================== clock ==================== */
+function displayTime() {
+    const clock = document.querySelector('.clock');
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    clock.innerHTML = time;
+    if (seconds < 10) {
+        clock.innerHTML = `${minutes}:0${seconds}`;
+    } else {
+        clock.innerHTML = `${minutes}:${seconds}`;
+    }
+}
+
+function startClock() {
+    clockId = setInterval(() => {
+        time++;
+        displayTime();
+    }, 1000);
+}
+
+function stopClock() {
+    clearInterval(clockId);
+}
+
+/* ==================== restGame ==================== */
+function resetTime() {
+    stopClock();
+    clockOff = true;
+    time = 0;
+    displayTime();
+}
+
+function resetMoves() {
+    moves = 0;
+    document.querySelector('.moves').innerHTML = moves;
+}
+
+function resetStar() {
+    stars = 0;
+    const starList = document.querySelectorAll('.stars li');
+    for (star of starList) {
+        star.style.display = 'inline';
+    }
+}
+
+function resetCards() {
+    const cards = document.querySelectorAll('.deck li');
+    for (let card of cards) {
+        card.className = 'card';
+    }
+}
+
+function resetGame() {
+    matched = 0;
+    toggleCards = [];
+    resetTime();
+    resetMoves();
+    resetStar();
+    shuffleDeck();
+    resetCards();
+}
+
+restart.addEventListener('click', resetGame);
+
+
+/* ==================== toggleCard ==================== */
 function toggleCard(clickTarget) {
     clickTarget.classList.toggle('open');
     clickTarget.classList.toggle('show');
@@ -57,150 +165,25 @@ function isClickValid(clickTarget) {
     );
 }
 
-////////////////////////////////////////////////////////////////
-/**
- * 
- * =====> shuffle
- **/
 
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-function shuffleDeck() {
-    const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
-    const shuffledCards = shuffle(cardsToShuffle);
-    for (const card of shuffledCards) {
-        deck.appendChild(card);
-    }
-}
-shuffleDeck();
-
-////////////////////////////////////////////////////////////////
-/**
- * 
- * =====> moves
- **/
-
-function addMove() {
-    moves++;
-    const movesText = document.querySelector('.moves');
-    movesText.innerHTML = moves;
-}
-
-////////////////////////////////////////////////////////////////
-/**
- * 
- * =====> removeStar
-**/
-
-function removeStar() {
-    const starList = document.querySelectorAll('.stars li');
-    for (const star of starList) {
-        if (star.style.display !== 'none') {
-            star.style.display = 'none'
-            break;
+deck.addEventListener('click', event => {
+    const clickTarget = event.target;
+    if(isClickValid(clickTarget)) {
+        if (clockOff) {
+            startClock();
+            clockOff = false;
+        }
+        toggleCard(clickTarget);
+        addToggleCard(clickTarget);
+        if(toggleCards.length === 2) {
+            checkForMatch(clickTarget);
+            addMove();
+            checkScore();
         }
     }
-}
+});
 
-function checkScore() {
-    if (moves === 16 || moves === 24) {
-        removeStar();
-    }
-}
-
-
-////////////////////////////////////////////////////////////////
-/**
- * 
- * =====> clock
- **/
-function startClock() {
-    clockId = setInterval(() => {
-        time++;
-        displayTime();
-    }, 1000);
-}
-
-function displayTime() {
-    const clock = document.querySelector('.clock');
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    clock.innerHTML = time;
-    if (seconds < 10) {
-        clock.innerHTML = `${minutes}:0${seconds}`;
-    } else {
-        clock.innerHTML = `${minutes}:${seconds}`;
-    }
-}
-
-function stopClock() {
-    clearInterval(clockId);
-}
-
-////////////////////////////////////////////////////////////////
-/**
- * 
- * =====> rest
- **/
-
-function resetClockAndTime() {
-    stopClock();
-    clockOff = true;
-    time = 0;
-    displayTime();
-}
-
-function resetMoves() {
-    moves = 0;
-    document.querySelector('.moves').innerHTML = moves;
-}
-
-function resetStar() {
-    stars = 0;
-    const starList = document.querySelectorAll('.stars li');
-    for (star of starList) {
-        star.style.display = 'inline';
-    }
-}
-
-function resetCards() {
-    const cards = document.querySelectorAll('.deck li');
-    for (let card of cards) {
-        card.className = 'card';
-    }
-}
-
-function resetGame() {
-    matched = 0;
-    toggleCards = [];
-    resetClockAndTime();
-    resetMoves();
-    resetStar();
-    shuffleDeck();
-    resetCards();
-}
-
-restart.addEventListener('click', resetGame);
-
-
-////////////////////////////////////////////////////////////////
-/**
- * 
- * =====> model
- **/
-
+/* ==================== popupModel ==================== */
 function toggleModal() {
     const model = document.querySelector('.modal-background');
     model.classList.toggle('hide');
@@ -229,8 +212,6 @@ function writeModalStats() {
     starsStat.innerHTML = `Stars = ${stars}`;
 }
 
-
-
 function gameOver() {
     if (matched === TOTAL_PAIRS) {
         stopClock();
@@ -249,23 +230,3 @@ cancel.addEventListener('click', () => {
 });
 
 replay.addEventListener('click', replayGame);
-
-////////////////////////////////////////////////////////////////
-
-deck.addEventListener('click', event => {
-    const clickTarget = event.target;
-    if(isClickValid(clickTarget)) {
-        if (clockOff) {
-            startClock();
-            clockOff = false;
-        }
-        toggleCard(clickTarget);
-        addToggleCard(clickTarget);
-        if(toggleCards.length === 2) {
-            checkForMatch(clickTarget);
-            addMove();
-            checkScore();
-        }
-    }
-});
-
